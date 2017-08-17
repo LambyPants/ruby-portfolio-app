2d4c868126b3ebe68747fc9fccc7b7e50065b8a4
Ryan Lambert <rllambert@rollins.edu>
2017-08-17 16:43:35 +0000

    integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,27 @@
 class PortfoliosController < ApplicationController
     
-    #GET /potfolios 
+    #GET /portfolios 
     def index
     @portfolio_items = Portfolio.all
     end
+    
+    #GET /portfolios/new
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+
+    #POST /portfolios
+    def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+     end
+    end
 end
+  
