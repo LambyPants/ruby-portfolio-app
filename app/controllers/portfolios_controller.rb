7fd544624ad87910bc0fbfca8b20f3f0e2dd9913
Ryan Lambert <rllambert@rollins.edu>
2017-08-24 18:04:46 +0000

    added delete routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,8 +41,19 @@ class PortfoliosController < ApplicationController
     end
     end
   end
+  #GET /portfolios/:id
   def show
     @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  #DELETE /portfolios/:id
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Record successfully removed.' }
+    end
   end
 end
   
