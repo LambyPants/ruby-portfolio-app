958e9626c3586b030ce0abe5af7309c2153fb425
LambyPants <Rllambert@rollins.edu>
2017-10-09 10:11:25 -0500

    install bootstrap 4

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
     layout "portfolio"
-    
+
     #GET /portfolios
     def index
     @portfolio_items = Portfolio.all
@@ -32,31 +33,25 @@ class PortfoliosController < ApplicationController
 
     # GET / portfolio/:id/edit
     def edit
-        @portfolio_item = Portfolio.find(params[:id])
         3.times { @portfolio_item.technologies.build }
     end
 
       # PATCH/PUT /portfolio/:id
   def update
-      @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
     if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated - YOU DID IT.' }
       else
         format.html { render :edit }
-
     end
     end
   end
   #GET /portfolios/:id
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   #DELETE /portfolios/:id
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
-
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'Record successfully removed.' }
@@ -70,5 +65,7 @@ class PortfoliosController < ApplicationController
                                     :body,
                                     technologies_attributes: [:name]
                                     )
+  def set_portfolio_item
+        @portfolio_item = Portfolio.find(params[:id])
   end
 end
