2675d89ee9da5880a08fb48d902062d2154052fc
LambyPants <Rllambert@rollins.edu>
2017-10-11 12:50:46 -0500

    prep for ruby gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,3 +6,16 @@ class ApplicationController < ActionController::Base
   include DefaultPageContent
 
 end
+
+def set_copyright
+  @copyright = RLambertViewTool::Renderer.copyright 'Ryan Lambert', 'All rights reserved'
+  end
+end
+
+module RLambertViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy: #{Time.now.year} | <b> #{name} </b> #{msg}".html_safe
+    end
+  end
+end
