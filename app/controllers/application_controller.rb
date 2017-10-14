867cb4614c4368618d9a9d2c46fa7c8d6409e139
Ryan Lambert <rllambert@rollins.edu>
2017-10-14 21:46:04 +0000

    generated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-end
-
-def set_copyright
-  @copyright = RLambertViewTool::Renderer.copyright 'Ryan Lambert', 'All rights reserved'
-  end
-end
-
-module RLambertViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy: #{Time.now.year} | <b> #{name} </b> #{msg}".html_safe
-    end
-  end
 end
